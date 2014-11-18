77beb02dbd7a66d127b6d4964ab11f94b79f52a3
Eli Williams <eliwilliams@Elis-MBP.home>
2014-11-17 22:47:04 -0500

    added bootstrap

--- lwc/src/lwc/settings.py
+++ lwc/src/lwc/settings.py
@@ -13,6 +13,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -75,6 +76,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+TEMPLATE_DIRS ={
+    os.path.join(BASE_DIR, 'templates'),
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
