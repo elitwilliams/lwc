3fba0a7bb0946b28313780dfefac8c2c1e88948b
elitwilliams <eli.t.williams@gmail.com>
2014-11-19 13:10:20 -0500

    updated joins with ref_id

--- lwc/src/joins/views.py
+++ lwc/src/joins/views.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,7 +7,27 @@ from django.shortcuts import render
 from .forms import EmailForm, JoinForm
 from .models import Join
 
+def get_ip(request):
+	try:
+		x_forward = request.META.get("HTTP_X_FORWARDED_FOR")
+		if x_forward:
+			ip = x_forward.split(",")[0]
+		else:
+			ip = request.META.get("REMOTE_ADDR")
+	except:
+		ip = ""
+	return ip
+
+def get_ref_id():
+	ref_id = str(uuid.uuid4())[:11].replace('-','').lower()
+	try:
+		id_exists = Join.objects.get(ref_id=ref_id)
+		get_ref_id()
+	except:
+		return ref_id
+
 def home(request):
+	
 	# # print request.POST["email"], request.POST["email_2"]
 
 # This is using regular Django Forms
@@ -20,9 +42,14 @@ def home(request):
 	form = JoinForm(request.POST or None)
 	if form.is_valid():
 		new_join = form.save(commit=False)
-		
 		email = form.cleaned_data['email']
-		new_join, created = Join.objects.get_or_create(email=email)
+		new_join_old, created = Join.objects.get_or_create(email=email)
+		if created:
+			new_join_old.ref_id = get_ref_id()
+			new_join_old.ip_address = get_ip(request)
+			new_join_old.save()
+		# redirect here	
+		# new_join.ip_address = get_ip(request)
 		# new_join.save()
 
 	context = {"form":form}
